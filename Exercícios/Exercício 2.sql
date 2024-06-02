11c3343c0dcfdb69fd0feb147be9e03ac7166b2b
Letícia Braga <leticiiabraga@hotmail.com>
2024-06-02 00:59:37 -0300

    Apagando a tabela de vendedores

---
+++ Exercícios/Exercício 2.sql
@@ -0,0 +1,11 @@
+-- Apague a tabela TABELA_DE_VENDEDORES2 usando script SQL.
+
+-- Para criar a tabela antes de apagá-la execute:
+CREATE TABLE TABELA_DE_VENDEDORES2 (
+        MATRICULA varchar(5),
+        NOME varchar(100),
+        PERCENTUAL_COMISSAO float
+);
+
+-- Após criar a tabela conforme o enunciado, irei apagar utilizando o comando:
+DROP TABLE TABELA_DE_VENDEDORES2;
