ce9cf8d1627379a4bcb74a33f4ca12ce32258585
Letícia Braga <leticiiabraga@hotmail.com>
2024-06-02 01:28:25 -0300

    Incluindo mais dois vendedores

---
+++ Exercícios/Exercício 4.sql
@@ -0,0 +1,18 @@
+-- Vamos criar mais dois vendedores, no mesmo comando SQL.
+-- Matrícula: 00235
+-- Nome: Márcio Almeida Silva
+-- Comissão: 8%COPIAR CÓDIGO
+
+-- Matrícula: 00236
+-- Nome: Cláudia Morais 
+-- Comissão: 8%
+
+INSERT INTO TABELA_DE_VENDEDORES
+(MATRICULA, NOME, PERCENTUAL_COMISSAO)
+VALUES
+('00235', 'Márcio Almeida Silva', 0.08);
+ 
+INSERT INTO TABELA_DE_VENDEDORES
+(MATRICULA, NOME, PERCENTUAL_COMISSAO)
+VALUES
+('00236', 'Cláudia Morais', 0.08);
